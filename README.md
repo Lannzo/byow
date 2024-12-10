2d18ededb9de335853ca0c829144123af344cd33
Pipluppp <duncanb013@gmail.com>
2024-12-11 02:49:01 +0800

    Implemented Drunkard Walk for world generation using random walks to adjacent tiles and smoothening isolated walls

--- README.md
+++ README.md
@@ -7,5 +7,7 @@ The official spec starts with a mode of interacting with the world of passing a 
 returning the state of the world after executing of said string. We can just start working directly on the interacive mode of playing 
 the game real-time through keyboard input.
 
-**Explore lab09 and lab10**
-These labs explore the rendering of the tiles for generating the world and the interactivity of the world, respectively
+**Explore lab09 and lab10**. These labs explore the rendering of the tiles for generating the world and the interactivity of the world, respectively
+
+The world generation algorithm from [Broguelike Dungeon Creation, Part 1](https://anderoonies.github.io/2020/03/17/brogue-generation.html)
+looks cool. No need to follow the world rules by the spec.
--- src/core/Main.java
+++ src/core/Main.java
@@ -1,10 +1,88 @@
 package core;
 
+import tileengine.TERenderer;
+import tileengine.TETile;
+import tileengine.Tileset;
+
+import java.util.ArrayDeque;
+import java.util.Random;
+
 public class Main {
+    private static final int WIDTH = 60;
+    private static final int HEIGHT = 45;
+    private static final int TILE_COUNT = WIDTH * HEIGHT;
+    private static final int CENTERX =  WIDTH / 2;
+    private static final int CENTERY =  HEIGHT / 2;
+
+
+
+    private static final long SEED = 2873123;
+    private static final Random RANDOM = new Random();
+
     public static void main(String[] args) {
+        // initialize the tile rendering engine with a window of size WIDTH x HEIGHT
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
 
-        // build your own world!
+        // initialize tiles
+        TETile[][] world = new TETile[WIDTH][HEIGHT];
+        randomWalk(world);
 
+        for (int i = 0; i < 3; i++) {
+        smoothen(world);
+        }
 
+        // draws the world to the screen
+        ter.renderFrame(world);
+    }
+
+    public static void randomWalk(TETile[][] tiles) {
+        int height = tiles[0].length;
+        int width = tiles.length;
+        int tileFloored = 0;
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                tiles[x][y] = Tileset.WALL;
+            }
+        }
+
+        int currentX = CENTERX;
+        int currentY = CENTERY;
+        while (tileFloored++ < TILE_COUNT * 0.5) {
+            tiles[currentX][currentY] = Tileset.FLOOR;
+
+            // Randomly move to adjacent tile
+            switch (RANDOM.nextInt(4)) {
+                case 0 -> currentX++;
+                case 1 -> currentX--;
+                case 2 -> currentY++;
+                default -> currentY--;
+            }
+
+            // restrict currentX and currentY within bounds
+            currentX = Math.max(0, Math.min(currentX, width - 1));
+            currentY = Math.max(0, Math.min(currentY, height - 1));
+        }
+    }
+    public static void smoothen(TETile[][] tiles) {
+        for (int y = 1; y < HEIGHT - 1; y++) {
+            for (int x = 1; x < WIDTH - 1; x++) {
+                if (tiles[x][y] == Tileset.WALL && isEdgeWalls(tiles, x, y)) {
+                    tiles[x][y] = Tileset.FLOOR;
+                }
+            }
+
+        }
+    }
+
+    private static boolean isEdgeWalls(TETile[][] tiles, int x, int y) {
+        int floorCount = 0;
+        if (tiles[x + 1][y] == Tileset.FLOOR) { floorCount++; }
+        if (tiles[x - 1][y] == Tileset.FLOOR) { floorCount++; }
+        if (tiles[x][y + 1] == Tileset.FLOOR) { floorCount++; }
+        if (tiles[x][y - 1] == Tileset.FLOOR) { floorCount++; }
+
+        return floorCount > 2;
     }
 }
